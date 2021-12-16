0c6f3d84667bdc8cfcac5001c3877d515d6f76ce
Crowdbotics <team@crowdbotics.com>
2021-12-16 03:51:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "blue_iris_cleaning_32447",
+  url: "https://blue_iris_cleaning_32447.botics.co",
+  api: "https://blue_iris_cleaning_32447.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
